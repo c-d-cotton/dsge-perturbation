91eefc1e8299e11fd0af35b04238620ef9730b5a
Christopher Cotton <Christopher.Cotton@bos.frb.org>
2026-03-19 10:52:03 -0400

    Fix bug with expssvarpath and replace with ssexpvarpath

--- simlineardsge_func.py
+++ simlineardsge_func.py
@@ -50,18 +50,27 @@ def simpathlinear_inputdict(inputdict):
     inputdict['varpath'] = simpathlinear(inputdict['gx'], inputdict['hx'], inputdict['shockpath'])
 
     # get list of steady states in order of states, shocks, controls
-    ssvec = np.array([inputdict['varfplusonlyssdict'][var] for var in inputdict['states'] + inputdict['shocks'] + inputdict['controls']])
+    # ssending2 is "_ss" - note that otherwise if I use loglineareqs then the variable will be 0
+    ssvec = np.array([inputdict['replacedict'][var + inputdict['ssending2']] for var in inputdict['states'] + inputdict['shocks'] + inputdict['controls']])
+
+    # old:{{{
+    # there was a mistake here before where I was computing ssvarpath to be xhat + xss and expssvarpath to be exp(xhat - xss)
+    # but normally what I want is xss * exp(xhat)
 
     # get x rather than xhat = x - xbar
     # note that [[0, 1], [2, 3]] + [2, 4] in numpy = [[2, 4], [5, 7]]
     # thus each column of the matrix is multiplied by the element of the vector
     # this is what I need since every column represents the path of a variable and I want to multiply it by the steady state
-    inputdict['ssvarpath'] = inputdict['varpath'] + ssvec
+    # inputdict['ssvarpath'] = inputdict['varpath'] + ssvec
 
     # if I log-linearized variables then I'll want to take the exponential i.e. X = exp(x)
     # note that if I did log-linearize then the ss above will be the ss of the log variable so I need to take the exponential of that as well
-    inputdict['expssvarpath'] = np.exp(inputdict['ssvarpath'])
+    # old:}}}
 
+    inputdict['expvarpath'] = np.exp(inputdict['varpath'])
+
+    # typically varpath captures xhat and what I actually want is xss * exp(xhat)
+    inputdict['ssexpvarpath'] = ssvec * inputdict['expvarpath']
 
     return(inputdict)
 
